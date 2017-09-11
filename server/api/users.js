b8cd70d4bb9b767b141c02348b19ab1a7b0748ad
David Berger <dgaberger@gmail.com>
2017-09-11 17:01:38 -0400

    begin api

--- server/api/users.js
+++ server/api/users.js
@@ -4,7 +4,7 @@ module.exports = router
 
 router.get('/', (req, res, next) => {
   User.findAll({
-    // explicitly select only the id and email fields - even though
+    // explicitly selects only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
     attributes: ['id', 'email']
