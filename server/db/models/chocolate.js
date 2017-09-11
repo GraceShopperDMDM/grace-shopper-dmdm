d1ab220646407c21355e4d8d443b7dae6ae47623
MaxAllen <maxton.allen@gmail.com>
2017-09-11 18:35:29 -0400

    added models pre test

---
+++ server/db/models/chocolate.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Chocolate = db.define('chocolate', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  category: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER
+  },
+  stock: {
+    type: Sequelize.INTEGER
+  },
+  photo: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Chocolate
---
+++ server/db/models/order.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('order', {
+  shipdate: {
+    type: Sequelize.DATE
+  },
+  deliverydate: {
+    type: Sequelize.DATE
+  },
+  status: {
+    type: Sequelize.ENUM('received', 'shipped', 'delivered')
+  }
+})
+
+module.exports = Order
