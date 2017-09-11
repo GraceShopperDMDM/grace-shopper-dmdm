9a9ba7f714d86dd81dd61057680d149fb7034eed
Doris Cheng <dorcheng@umich.edu>
2017-09-11 15:14:36 -0400

    boilerplate

---
+++ seed.js
@@ -0,0 +1,15 @@
+const db = require('./server/db');
+const { User } = require('./server/db/models');
+
+db.sync({force: true})
+  .then(() => {
+    return User.create({name: 'Cody'});
+  })
+  .then(() => {
+    db.close();
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  });
