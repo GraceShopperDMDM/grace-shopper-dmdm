645c864ab3cc7a12ee7e09a55a290f55189669ef
MaxAllen <maxton.allen@gmail.com>
2017-09-18 17:18:05 -0400

    cart backend

--- client/components/UserCart.js
+++ client/components/UserCart.js
@@ -1,48 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { fetchCart } from '../store'
 
-export const UserCart = (props) => {
-  const products = props.products
-  return (
-    <div>
-      <h3>My Cart</h3>
-      <table>
-        <tbody>
-          <tr>
-            <th>Remove</th>
-            <th>Products</th>
-            <th></th>
-            <th>Qunatity</th>
-            <th>Total</th>
-          </tr>
-          {
-            products.map(product =>
-              <tr key={product.id}>
-                <td>
-                  <button>Remove</button>
-                </td>
-                <td>
-                  <img src={product.photo} />
-                </td>
-                <td>
-                  <Link to={`/products/${product.id}`} key={product.id}>
-                    {product.name}
-                  </Link>
-                </td>
-                <td>
-                  <input />
-                </td>
-                <td>
-                  {product.price}
-                </td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
-    </div>
-  )
+class UserCart extends Component {
+  // constructor (props) {
+  //   super(props)
+  // }
+
+  ComponentWillMount () {
+    console.log('mounted')
+    this.props.fetchCart()
+  }
+
+  render () {
+    console.log('we are here!')
+    const products = this.props.products
+
+    return (
+      <div>
+        <h3>My Cart</h3>
+        <table>
+          <tbody>
+            <tr>
+              <th>Remove</th>
+              <th>Products</th>
+              <th></th>
+              <th>Qunatity</th>
+              <th>Total</th>
+            </tr>
+            {
+              products.map(product =>
+                <tr key={product.id}>
+                  <td>
+                    <button>Remove</button>
+                  </td>
+                  <td>
+                    <img src={product.photo} />
+                  </td>
+                  <td>
+                    <Link to={`/products/${product.id}`} key={product.id}>
+                      {product.name}
+                    </Link>
+                  </td>
+                  <td>
+                    <input />
+                  </td>
+                  <td>
+                    {product.price}
+                  </td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 const mapState = (state) => {
@@ -52,4 +66,12 @@ const mapState = (state) => {
   }
 }
 
-export default connect(mapState)(UserCart)
+const mapDispatch = (dispatch, ownProps) => {
+  return {
+    loadCart () {
+      dispatch(fetchCart(ownProps.match.params.id))
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(UserCart)
