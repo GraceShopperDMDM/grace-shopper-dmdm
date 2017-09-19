80ef0225d4db9f86a89aee69e81a07d8a387da7d
David Berger <dgaberger@gmail.com>
2017-09-19 16:50:35 -0400

    betterized the seed.js

--- seed.js
+++ seed.js
@@ -1,32 +1,76 @@
 const db = require('./server/db')
 const { User, Order, Chocolate, Review, ChocolateOrder } = require('./server/db/models')
 
-const users = [{
-  username: 'Cody',
-  email: 'cody@cody.com',
-  address: '111 some st., NY, 11000',
-  password: '123',
-  isAdmin: 'false'
-}, {
-  username: 'Garry',
-  email: 'garry@garry.com',
-  address: '122 some ave., NY, 11300',
-  password: '123',
-  isAdmin: 'true'
-}]
+const users = [
+  {
+    username: 'Cody',
+    email: 'cody@cody.com',
+    address: '111 some st., NY, 11000',
+    password: '123',
+    isAdmin: 'false'
+  }, {
+    username: 'Garry',
+    email: 'garry@garry.com',
+    address: '122 some ave., NY, 11300',
+    password: '123',
+    isAdmin: 'true'
+  }, {
+    username: 'Larry',
+    email: 'larry@larry.com',
+    address: '124 some ave., NY, 11500',
+    password: '123',
+    isAdmin: 'true'
+  }, {
+    username: 'Terry',
+    email: 'terry@terry.com',
+    address: '125 some ave., NY, 12500',
+    password: '123',
+    isAdmin: 'false'
+  }
+]
 
 const chocolates = [{
-  name: 'rocky road',
+  name: 'Rocky Road',
   category: 'ice cream',
-  description: 'very delicious!',
+  description: 'Very delicious!',
+  photo: 'https://cdn.pixabay.com/photo/2014/01/16/00/51/ice-cream-246161__340.jpg',
   price: 10.25,
   stock: 15
 }, {
-  name: 'dark chocolate fudge',
+  name: 'Dark Chocolate Fudge',
   category: 'fudge',
-  description: 'very good!',
+  description: 'Very good!',
+  photo: 'https://cdn.pixabay.com/photo/2015/12/07/17/49/fudge-1081100__340.jpg',
   price: 12.50,
-  stock: 20
+  stock: 18
+}, {
+  name: 'Chocolate Shavings',
+  category: 'dark',
+  description: 'Literally just shavings of chocolate.',
+  photo: 'https://cdn.pixabay.com/photo/2017/04/12/16/56/chocolate-2224998__340.jpg',
+  price: 9.95,
+  stock: 2000
+}, {
+  name: 'White Chocolate',
+  category: 'white',
+  description: 'Still Counts!',
+  photo: 'https://cdn.pixabay.com/photo/2014/06/30/20/05/white-chocolate-380702__340.jpg',
+  price: 14.40,
+  stock: 101
+}, {
+  name: 'Nutty Dark',
+  category: 'dark',
+  description: 'So Dark, much Nutty!',
+  photo: 'https://cdn.pixabay.com/photo/2013/09/18/18/24/chocolate-183543__340.jpg',
+  price: 3.98,
+  stock: 4
+}, {
+  name: 'Hand-Churned Chocolate Ice Cream',
+  category: 'ice cream',
+  description: 'Churned up a hill in the snow, both ways!',
+  photo: 'https://cdn.pixabay.com/photo/2014/02/08/11/28/bun-261677__340.jpg',
+  price: 11.89,
+  stock: 29
 }]
 
 const orders = [{
@@ -43,12 +87,12 @@ const orders = [{
 
 const reviews = [{
   stars: '5',
-  body: 'this is great!',
+  body: 'This is great! Also the earth is flat',
   chocolateId: 1,
   userId: 1
 }, {
   stars: '1',
-  body: 'this is terrible!',
+  body: 'This is terrible! Whoever made this should be sad.',
   chocolateId: 2,
   userId: 2
 }]
